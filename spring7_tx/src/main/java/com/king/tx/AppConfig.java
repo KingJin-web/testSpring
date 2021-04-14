ef455ada89ca39935c8dde34ebb6e08d5c3e8207
12613 <1261337209@qq.com>
2021-04-15 00:51:40 +0800

    spring jdbc

---
+++ spring7_tx/src/main/java/com/king/tx/AppConfig.java
@@ -0,0 +1,32 @@
+package com.king.tx;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.beans.PropertyVetoException;
+
+/**
+ * @program: testSpring
+ * @description:
+ * @author: King
+ * @create: 2021-04-14 19:23
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.king"})
+public class AppConfig {
+
+    @Bean
+    public ComboPooledDataSource dataSource() throws PropertyVetoException {
+        DataSource ds = new ComboPooledDataSource();
+        ((ComboPooledDataSource) ds).setDriverClass("com.mysql.cj.jdbc.Driver");
+        ((ComboPooledDataSource) ds).setJdbcUrl("jdbc:mysql://localhost:3306/mybatis?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=UTF-8");
+        ((ComboPooledDataSource) ds).setUser("root");
+        ((ComboPooledDataSource) ds).setPassword("aaaa");
+        return (ComboPooledDataSource) ds;
+    }
+}
