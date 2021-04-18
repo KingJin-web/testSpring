a36577391e00d5fba2a51f265936a52f34cf38fa
12613 <1261337209@qq.com>
2021-04-18 20:32:07 +0800

    改错

---
+++ printbook_bank/src/test/java/com/king/bank/PrintbookBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintbookBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
