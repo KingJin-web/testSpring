f1fc9bf36df11bcb327531ac2e441a7047020341
12613 <1261337209@qq.com>
2021-04-18 09:57:44 +0800

    springboot_HelloWorld

---
+++ springboot_helloworld/src/main/java/com/king/springboot/springboot/SpringbootHelloworldApplication.java
@@ -0,0 +1,15 @@
+package com.king.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHelloworldApplication.class, args);
+    }
+
+}
+// 2021-04-18 09:47:50.110  INFO 24616 --- [           main] c.k.s.s.SpringbootHelloworldApplication  : Starting SpringbootHelloworldApplication using Java 1.8.0_271 on DESKTOP-LCJBD6D with PID 24616 (H:\jetbrains\java\testSpring\springboot_helloworld\target\classes started by King in H:\jetbrains\java\testSpring)
+
