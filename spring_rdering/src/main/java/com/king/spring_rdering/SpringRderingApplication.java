88d91e5a09324c542c23891fd5ccc2fbb7b67c69
12613 <1261337209@qq.com>
2021-04-29 18:32:19 +0800

    小萌神订餐网

---
+++ spring_rdering/src/main/java/com/king/spring_rdering/SpringRderingApplication.java
@@ -0,0 +1,13 @@
+package com.king.spring_rdering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRderingApplication.class, args);
+    }
+
+}
---
+++ spring_rdering/src/test/java/com/king/spring_rdering/SpringRderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.spring_rdering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
